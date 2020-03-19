1f4f0bcc5ae16105b524e7456cd397f6d0193c8c
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-03-19 08:39:28 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,33 @@
 class Pokemon
+  
+  attr_accessor :id, :name, :type, :db
+  
+  def initialize(name:,type:, id:, db:)
+    @name = name 
+    @type = type 
+    @id = id
+    @db = db
+  end
+  
+  def self.save(name, type, db)
+    sql = <<-SQL
+      INSERT INTO pokemon (name, type)
+      VALUES (?, ?)
+      
+    SQL
+    db.execute(sql, name, type)
+  end
+  
+  def self.find(id, db)
+    sql = <<-SQL 
+      SELECT * FROM pokemon 
+      WHERE id =?
+    SQL
+
+    pokemon = db.execute(sql, id)
+    
+    new_pokemon = Pokemon.new(id: id, name: pokemon[0][1], type: pokemon[0][2], db: db)
+    new_pokemon
+  end
+  
 end
