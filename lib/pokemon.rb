555a502fd5ce321fdfea9cc9d17533b8f0a96762
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-03-19 23:10:13 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,13 +2,18 @@ class Pokemon
   
   attr_accessor :id, :name, :type, :db
   
+<<<<<<< HEAD
   def initialize(name:,type:, id:, db:)
+=======
+  def initialize(name,type, id= nil, db)
+>>>>>>> 778a08ebc9a462be1e84ae08ddf8de98f171a79b
     @name = name 
     @type = type 
     @id = id
     @db = db
   end
   
+<<<<<<< HEAD
   def self.save(name, type, db)
     sql = <<-SQL
       INSERT INTO pokemon (name, type)
@@ -28,6 +33,18 @@ class Pokemon
     
     new_pokemon = Pokemon.new(id: id, name: pokemon[0][1], type: pokemon[0][2], db: db)
     new_pokemon
+=======
+  def self.save 
+    sql = <<-SQL
+      INSERT INTO pokemon (name, type)
+      VALUES name = ?, type = ?
+      
+    SQL
+    db.execute(sql, self.name, self.type)
+  end
+  
+  def self.find 
+>>>>>>> 778a08ebc9a462be1e84ae08ddf8de98f171a79b
   end
   
 end
